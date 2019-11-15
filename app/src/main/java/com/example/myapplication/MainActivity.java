1222c7a743abaab9496d5bad4e0e0e6d7e9668da
samhitha <samhitha.pyla@loictreeit.in>
2019-11-15 11:47:31 +0530

    branch commitd

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String stage = "stag gvygvcgyue";
+    private String stage = "stag bb samhitha";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
