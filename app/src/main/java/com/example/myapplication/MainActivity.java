b8916c4c944a2a478820a14ccd1612d6d4041745
samhitha <samhitha.pyla@loictreeit.in>
2019-11-15 12:21:36 +0530

    test

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     //hello
+    String abc="sam";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
