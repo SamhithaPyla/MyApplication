b9b8a3f2d2a96851e5557360af99c68ae9d059ea
samhitha <samhitha.pyla@loictreeit.in>
2019-11-15 11:49:09 +0530

    string

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,9 +6,14 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    String a = "A";
+    String b = "B";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
